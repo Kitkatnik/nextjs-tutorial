12b355857ebaca6f11adfba65b477ff0801a48f1
Katya <kittyserenakat@gmail.com>
2023-02-06 14:28:37 -0500

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Head from 'next/head'
+import Link from 'next/link';
+
+import Date from '../components/date';
 import Layout, { siteTitle } from '../components/layout'
+
 import utilStyles from '../styles/utils.module.css'
+
 import { getSortedPostsData } from '../lib/posts'
 
 export default function Home({ allPostsData }) {
@@ -17,12 +22,12 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
           ))}
         </ul>
       </section>
